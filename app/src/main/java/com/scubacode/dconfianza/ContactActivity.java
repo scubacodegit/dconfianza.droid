b4b8e8f144391b9e721aae5d16b58e81ba1c1de4
scubacodegit <dev@scubacode.com>
2017-02-16 20:26:00 -0600

    applying styles to activity_contact.xml

--- app/src/main/java/com/scubacode/dconfianza/ContactActivity.java
+++ app/src/main/java/com/scubacode/dconfianza/ContactActivity.java
@@ -39,219 +39,226 @@ public class ContactActivity extends ActivitySecure
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if(!userIsLoggedIn)
+        try
         {
-            finish();
-            this.LoginRedirect();
-            return;
-        }
-        setContentView(R.layout.activity_contact);
-        showToolbar(null, false);
-        final Button submit = (Button) findViewById(R.id.button_submit);
-
-        name = (TextInputEditText) findViewById(R.id.input_edit_text_first_name);
-        email= (TextInputEditText) findViewById(R.id.input_edit_text_email);
-        message= (TextInputEditText) findViewById(R.id.input_edit_text_message);
-
-        firstNameInputLayout = (TextInputLayout) findViewById(R.id.input_layout_first_name);
-        emailInputLayout = (TextInputLayout) findViewById(R.id.input_layout_email);
-        messageInputLayout = (TextInputLayout) findViewById(R.id.input_layout_message);
-
-        name.addTextChangedListener(new TextWatcher()
-        {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
+            super.onCreate(savedInstanceState);
+            if(!userIsLoggedIn)
             {
-                try
+                finish();
+                this.LoginRedirect();
+                return;
+            }
+            setContentView(R.layout.activity_contact);
+
+            final Button submit = (Button) findViewById(R.id.button_submit);
+
+            name = (TextInputEditText) findViewById(R.id.input_edit_text_first_name);
+            email= (TextInputEditText) findViewById(R.id.input_edit_text_email);
+            message= (TextInputEditText) findViewById(R.id.input_edit_text_message);
+
+            firstNameInputLayout = (TextInputLayout) findViewById(R.id.input_layout_first_name);
+            emailInputLayout = (TextInputLayout) findViewById(R.id.input_layout_email);
+            messageInputLayout = (TextInputLayout) findViewById(R.id.input_layout_message);
+
+            name.addTextChangedListener(new TextWatcher()
+            {
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count)
                 {
-                    firstNameInputLayout.setErrorEnabled(false);
-                    firstNameInputLayout.setError(null);
-                }
-                catch(Exception ex)
-                {
-                    handleException(ex,true);
-                }
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after)
-            {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)
-            {
-
-            }
-
-        });
-
-        email.addTextChangedListener(new TextWatcher()
-        {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                try
-                {
-                    emailInputLayout.setErrorEnabled(false);
-                    emailInputLayout.setError(null);
-                }
-                catch(Exception ex)
-                {
-                    handleException(ex,true);
-                }
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after)
-            {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)
-            {
-
-            }
-
-        });
-
-        message.addTextChangedListener(new TextWatcher()
-        {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count)
-            {
-                try
-                {
-                    messageInputLayout.setErrorEnabled(false);
-                    messageInputLayout.setError(null);
-                }
-                catch(Exception ex)
-                {
-                    handleException(ex,true);
-                }
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after)
-            {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)
-            {
-
-            }
-
-        });
-
-        submit.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                try
-                {
-
-                    //validate
-
-                    if ( StringHelper.isNullOrEmpty(name.getText().toString()))
+                    try
                     {
-                        firstNameInputLayout.setErrorEnabled(true);
-                        firstNameInputLayout.setError(getString(R.string.misc_required_field));
-                        name.requestFocus();
-                        return;
+                        firstNameInputLayout.setErrorEnabled(false);
+                        firstNameInputLayout.setError(null);
+                    }
+                    catch(Exception ex)
+                    {
+                        handleException(ex,true);
                     }
 
-                    if (StringHelper.isNullOrEmpty(email.getText().toString()))
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after)
+                {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s)
+                {
+
+                }
+
+            });
+
+            email.addTextChangedListener(new TextWatcher()
+            {
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count)
+                {
+                    try
                     {
-                        emailInputLayout.setErrorEnabled(true);
-                        emailInputLayout.setError(getString(R.string.misc_required_field));
-                        email.requestFocus();
-                        return;
+                        emailInputLayout.setErrorEnabled(false);
+                        emailInputLayout.setError(null);
+                    }
+                    catch(Exception ex)
+                    {
+                        handleException(ex,true);
                     }
 
-                    if (!Email.isValid(email.getText().toString()) )
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after)
+                {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s)
+                {
+
+                }
+
+            });
+
+            message.addTextChangedListener(new TextWatcher()
+            {
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count)
+                {
+                    try
                     {
-                        emailInputLayout.setErrorEnabled(true);
-                        emailInputLayout.setError(getString(R.string.reg_invalid_email));
-                        email.requestFocus();
-                        return;
+                        messageInputLayout.setErrorEnabled(false);
+                        messageInputLayout.setError(null);
+                    }
+                    catch(Exception ex)
+                    {
+                        handleException(ex,true);
                     }
 
-                    if ( StringHelper.isNullOrEmpty(message.getText().toString()))
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after)
+                {
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s)
+                {
+
+                }
+
+            });
+
+            submit.setOnClickListener(new View.OnClickListener()
+            {
+                public void onClick(View v)
+                {
+                    try
                     {
-                        messageInputLayout.setErrorEnabled(true);
-                        messageInputLayout.setError(getString(R.string.misc_required_field));
-                        name.requestFocus();
-                        return;
-                    }
 
+                        //validate
 
-                    final ProgressDialog waitDialog = ProgressDialog.show(ContactActivity.this,
-                            getString(R.string.app_name),
-                            getString(R.string.misc_please_wait));
-
-                    ContactMessage contactMessage = new ContactMessage();
-                    contactMessage.setUserID(session.getUser().getUserID());
-                    contactMessage.setEmail(email.getText().toString());
-                    contactMessage.setMessage(message.getText().toString());
-
-                    Call<Integer> call = ApiAdapter.getApiService().addMessage(contactMessage);
-                    call.enqueue(new Callback<Integer>()
-                    {
-                        @Override
-                        public void onResponse(Call<Integer> call, Response<Integer> response)
+                        if ( StringHelper.isNullOrEmpty(name.getText().toString()))
                         {
-                            waitDialog.cancel();
+                            firstNameInputLayout.setErrorEnabled(true);
+                            firstNameInputLayout.setError(getString(R.string.misc_required_field));
+                            name.requestFocus();
+                            return;
+                        }
 
-                            AlertDialog dialog = alertBox(
-                                    getString(R.string.main_message_received),
-                                    getString(R.string.misc_ok),
-                                    new DialogInterface.OnClickListener()
-                                    {
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            try
-                                            {
-                                                dialog.dismiss();
-                                                //limpiar forma
-                                                refreshForm();
-                                            }
-                                            catch(Exception ex)
-                                            {
-                                                handleException(ex,true);
+                        if (StringHelper.isNullOrEmpty(email.getText().toString()))
+                        {
+                            emailInputLayout.setErrorEnabled(true);
+                            emailInputLayout.setError(getString(R.string.misc_required_field));
+                            email.requestFocus();
+                            return;
+                        }
+
+                        if (!Email.isValid(email.getText().toString()) )
+                        {
+                            emailInputLayout.setErrorEnabled(true);
+                            emailInputLayout.setError(getString(R.string.reg_invalid_email));
+                            email.requestFocus();
+                            return;
+                        }
+
+                        if ( StringHelper.isNullOrEmpty(message.getText().toString()))
+                        {
+                            messageInputLayout.setErrorEnabled(true);
+                            messageInputLayout.setError(getString(R.string.misc_required_field));
+                            name.requestFocus();
+                            return;
+                        }
+
+
+                        final ProgressDialog waitDialog = ProgressDialog.show(ContactActivity.this,
+                                getString(R.string.app_name),
+                                getString(R.string.misc_please_wait));
+
+                        ContactMessage contactMessage = new ContactMessage();
+                        contactMessage.setUserID(session.getUser().getUserID());
+                        contactMessage.setEmail(email.getText().toString());
+                        contactMessage.setMessage(message.getText().toString());
+
+                        Call<Integer> call = ApiAdapter.getApiService().addMessage(contactMessage);
+                        call.enqueue(new Callback<Integer>()
+                        {
+                            @Override
+                            public void onResponse(Call<Integer> call, Response<Integer> response)
+                            {
+                                waitDialog.cancel();
+
+                                AlertDialog dialog = alertBox(
+                                        getString(R.string.main_message_received),
+                                        getString(R.string.misc_ok),
+                                        new DialogInterface.OnClickListener()
+                                        {
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                try
+                                                {
+                                                    dialog.dismiss();
+                                                    //limpiar forma
+                                                    refreshForm();
+                                                }
+                                                catch(Exception ex)
+                                                {
+                                                    handleException(ex,true);
+                                                }
                                             }
                                         }
-                                    }
-                            );
-                            dialog.show();
-                        }
+                                );
+                                dialog.show();
+                            }
 
-                        @Override
-                        public void onFailure(Call<Integer> call, Throwable t)
-                        {
-                            waitDialog.cancel();
-                            handleException(t.getMessage(),true);
-                        }
+                            @Override
+                            public void onFailure(Call<Integer> call, Throwable t)
+                            {
+                                waitDialog.cancel();
+                                handleException(t.getMessage(),true);
+                            }
 
-                    });
+                        });
+                    }
+                    catch(Exception ex)
+                    {
+                        handleException(ex,true);
+                    }
+
                 }
-                catch(Exception ex)
-                {
-                    handleException(ex,true);
-                }
+            });
 
-            }
-        });
-
+        }
+        catch(Exception ex)
+        {
+            handleException(ex,true);
+        }
     }
 
     private void refreshForm()
